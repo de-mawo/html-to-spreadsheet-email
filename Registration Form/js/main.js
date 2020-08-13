be5ad10eb4fc33717615b8e8d102a3f4e69b7c05
groovebasso <64476470+groovebass@users.noreply.github.com>
2020-08-13 13:03:22 +0200

    Update main.js

--- Registration Form/js/main.js
+++ Registration Form/js/main.js
@@ -9,25 +9,8 @@ const scriptURL = 'https://script.google.com/macros/s...........'
         })
         .then(response => console.log('Success!', response))
         .catch(error => console.error('Error!', error.message))
+        .finally(window.location.replace("https://dreamfactoryfoundation.org/register"))
 
-       if (response) {
-           // alert("hip")
-           window.location.reload();
-       }
+       
     });
 
-//Form not auto-refreshing , hence put Alerts to inform the user to manually refresh after clicking submit
-
-    let btnClear = document.getElementById('btn')
-    let inputs = document.querySelectorAll('input')
-
-     btnClear.addEventListener('click', () => {
-         alert('Your Information is submitted Successfully. Please DO NOT CLICK *SUBMIT* again .Click: REFRESH BUTTON, Check your mail for the registration link');
-              alert('Click: REFRESH BUTTON, Check your mail for the registration link')
-
-
-
-     })
-
-
-// inputs.forEach(input => input.value ='');
